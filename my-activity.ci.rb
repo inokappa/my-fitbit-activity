ddbe64a489d4accc4b4d21489d34094caacab6f9
inokappa <inokara@gmail.com>
2020-01-04 21:29:09 +0900

    remove loop method

--- my-activity.ci.rb
+++ my-activity.ci.rb
@@ -141,13 +141,10 @@ end
 auth = FitBitAuth.new
 access_token = ENV['FITBIT_ACCESS_TOKEN']
 
-loop do
-  logger.debug('Initial Access Token: ' + access_token)
-  access_token = auth.refresh_token unless auth.token_active?(access_token)
-  d = Date.today - 1
-  logger.debug('Using Access Token: ' + access_token)
-  dis = FitBitActivity.new(access_token, d.strftime("%Y-%m-%d")).distance
-  logger.info('Distance(km): ' + dis)
-  sleep(300)
-end
+logger.debug('Initial Access Token: ' + access_token)
+access_token = auth.refresh_token unless auth.token_active?(access_token)
+d = Date.today - 1
+logger.debug('Using Access Token: ' + access_token)
+dis = FitBitActivity.new(access_token, d.strftime("%Y-%m-%d")).distance
+logger.info('Distance(km): ' + dis)
 puts Pixela.new(ENV['PIXELA_GRAPH'], d.strftime("%Y%m%d")).post(dis.to_i)
